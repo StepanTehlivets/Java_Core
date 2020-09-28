fc99799c4d923b7c376077b9e1fd9d8a97f827be
stept <stept@DESKTOP-NHVGS76>
2020-09-28 22:29:41 +0200

    Small changes

--- lesson04/src/ua/academy/lgs2/Overload.java
+++ lesson04/src/ua/academy/lgs2/Overload.java
@@ -2,13 +2,16 @@ package ua.academy.lgs2;
 
 public class Overload {
 	private String name;
+	private String lastName;
 	private int age;
+
 	
-	Overload(){
-		this("Іван", 15);
+	Overload(String name, String last_name, int age){
+		this("Іван", "Мураха");
+		this.age=age;
 	}
 
-	Overload(String name, int age){
+	Overload(String name, String last_name){
 		this.name=name;
 		this.age=age;
 	}
